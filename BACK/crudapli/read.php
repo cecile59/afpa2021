4c976efa183fd90082d0daa96a14619f63cf3560
siminski cecile <cecile.siminski.pro@gmail.com>
2021-06-29 16:26:37 +0200

    crud

---
+++ BACK/crudapli/read.php
@@ -0,0 +1,20 @@
+<?php
+include 'functions.php';
+// Connect to MySQL database
+$pdo = pdo_connect_mysql();
+// Get the page via GET request (URL param: page), if non exists default the page to 1
+$page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
+// Number of records to show on each page
+$records_per_page = 5;
+
+// Prepare the SQL statement and get records from our contacts table, LIMIT will determine the page
+$stmt = $pdo->prepare('SELECT * FROM contacts ORDER BY id LIMIT :current_page, :record_per_page');
+$stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
+$stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
+$stmt->execute();
+// Fetch the records so we can display them in our template.
+$contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Get the total number of contacts, this is so we can determine whether there should be a next and previous button
+$num_contacts = $pdo->query('SELECT COUNT(*) FROM contacts')->fetchColumn();
+?>
---
+++ BACK/php/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php 
+  //lesson01
+    // $mavariable = "cecile" ;
+    // echo $mavariable ;
+
+    //lesson02
+    // echo"<h1>Bonjour le monde</h1>"; 
+// concatenation
+// echo "Affichage d'un \" "; 
+
+// $couleur[0] = "red";
+// $couleur[1] = "blue";
+// $couleur[2] = "white";
+// $couleur[3] = "black";
+// echo $couleur[2]
+
+
+// $_COOKIE["societe"] = "Afpa";
+// echo $_COOKIE['societe"]; // Affiche 'Afpa' ;
+
+// $a = 15.125863;
+//  settype($a, "integer"); 
+// echo $a; 
+
+// $a = 6.32172; 
+// $b = intval($a); 
+// $c = doubleval($a); 
+// echo $a - $b - $c; 
+
+// $a = "Winter"; 
+// echo $a . " is coming !"; 
+  // 
+//   for ($a = 1; $a <= 10; $a++) { 
+//     echo $a."<br>"; 
+// } 
+
+// $a = 0; 
+
+// do { 
+//     echo $a."<br>";  
+// } while ($a > 0);
+
+// (ne fonctionne pas ) 
+// do { 
+//   if ($a < 5) { 
+//       echo "a inférieur à 5"; 
+//       break; 
+//   } 
+//   if ( ) ....... 
+// } while (1); 
+
+// ------------------------- PHP- Les boucles EXO 1 / 2 / -----------------------
+// for ($a = 1; $a < 150; ($a =$a +2) ) { 
+//   echo $a."<br>"; 
+// } 
+
+// for ($a = 1; $a < 500; $a++) { 
+//   echo "<br>".$a."Je dois faire des sauvegardes régulières de mes fichiers."; 
+// } 
+
+  ?>
+</body>
+
+</html>
